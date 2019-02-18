9e89c8ea7c3191bf8b6612d8a49716afba003dc0
WZHrunningman <2238912552@qq.com>
2019-02-18 16:08:26 +0800

    上锁结算界面

---
+++ app/src/main/java/com/example/wzh/bigbigshe/fragment/SettleFragment.java
@@ -0,0 +1,30 @@
+package com.example.wzh.bigbigshe.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.wzh.bigbigshe.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettleFragment extends Fragment {
+
+
+    public SettleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settle, container, false);
+    }
+
+}
