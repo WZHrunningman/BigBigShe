6faf5a85f30a4fbd07e7c848a389b730b37735ef
WZHrunningman <2238912552@qq.com>
2019-02-19 16:37:10 +0800

    第12次上传 地图高精度定位

--- app/src/main/java/com/example/wzh/bigbigshe/fragment/HomeFragment.java
+++ app/src/main/java/com/example/wzh/bigbigshe/fragment/HomeFragment.java
@@ -2,18 +2,58 @@ package com.example.wzh.bigbigshe.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.model.MyLocationStyle;
 import com.example.wzh.bigbigshe.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
 
+
+    @BindView(R.id.map_View)
+    MapView mapView;
+    Unbinder unbinder;
+
+    AMap aMap;
+    MyLocationStyle myLocationStyle;
+
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
+    //声明定位回调监听器-异步获取定位结果
+    public AMapLocationListener mLocationListener = new AMapLocationListener() {
+        @Override
+        public void onLocationChanged(AMapLocation aMapLocation) {
+            if (aMapLocation.getErrorCode()==0) {
+                aMapLocation.getLatitude();//纬度
+                aMapLocation.getLongitude();//经度
+                aMapLocation.getAddress();
+                aMapLocation.getAccuracy();//精度信息
+                aMapLocation.getLocationType();//定位来源
+                onLocationChanged(aMapLocation);
+
+            }
+        }
+    };
 
     public HomeFragment() {
         // Required empty public constructor
@@ -24,7 +64,82 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        mapView.onCreate( savedInstanceState );       //创建地图
+        //地图的显示
+        if (aMap == null) {
+            aMap = mapView.getMap();
+        }
+        //定位蓝点
+        myLocationStyle = new MyLocationStyle();
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationEnabled(true);
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
+        //定位初始化
+        mLocationClient = new AMapLocationClient(getContext());
+        //定位回调监听
+        mLocationClient.setLocationListener(mLocationListener);
+
+        init();
+        return view;
     }
 
+    private void init() {
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        //高精度定位模式：会同时使用网络定位和GPS定位，优先返回最高精度的定位结果，以及对应的地址描述信息。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        //设置单次定位
+        //获取一次定位结果：
+        //该方法默认为false。
+        mLocationOption.setOnceLocation(true);
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+
+        //设置是否允许模拟位置,默认为true，允许模拟位置
+        mLocationOption.setMockEnable(true);
+        //给定位客户端对象设置定位参数
+        mLocationClient.setLocationOption(mLocationOption);
+        //启动定位
+        mLocationClient.startLocation();
+    }
+
+    //生命周期
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mapView.onDestroy();
+        unbinder.unbind();
+        mLocationClient.onDestroy();
+    }
+
+    //重新配置，加载地图
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mLocationClient.stopLocation();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    //保存地图当前的状态
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState( outState );
+        mapView.onSaveInstanceState( outState );
+    }
 }
